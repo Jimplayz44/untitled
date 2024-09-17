6db350a60d79b74dc5ba072e7ccd70997e728fb4
Jimplayz44 <jimplayz11@gmail.com>
2024-09-17 15:00:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@ input.onButtonPressed(Button.A, function () {
 })
 finch.startFinch()
 basic.forever(function () {
-    if (finch.getLine(RLDir.Right) > 90) {
-        finch.setMove(MoveDir.Backward, 10, 100)
-    } else if (finch.getLine(RLDir.Left) > 20) {
+    if (finch.getLine(RLDir.Right) < 90 && finch.getLine(RLDir.Left) > 20) {
         finch.setMove(MoveDir.Forward, 10, 100)
-    } else if (false) {
-    	
-    } else if (false) {
-    	
+    } else if (finch.getLine(RLDir.Right) > 90 && finch.getLine(RLDir.Left) > 90) {
+        finch.setMove(MoveDir.Backward, 10, 100)
+    } else if (finch.getLine(RLDir.Right) > 90 && finch.getLine(RLDir.Left) < 20) {
+        finch.setTurn(RLDir.Left, 45, 100)
+    } else if (finch.getLine(RLDir.Left) > 90 && finch.getLine(RLDir.Right) < 20) {
+        finch.setTurn(RLDir.Right, 45, 100)
     }
 })
